b0a448939db4e06d400b5e3fd14b1cdc763264e8
Sumant Mishra <sumantmishra511@gmail.com>
2020-08-28 21:07:11 +0530

    notes and view syllabus added
    refactoring
    
    conflict resolve
    
    mobile add removed

--- src/app/+student/student-landing-page/student-landing-page.component.ts
+++ src/app/+student/student-landing-page/student-landing-page.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-student-landing-page',
-  templateUrl: './student-landing-page.component.html',
-  styleUrls: ['./student-landing-page.component.css']
+  selector: "app-student-landing-page",
+  templateUrl: "./student-landing-page.component.html",
+  styleUrls: ["./student-landing-page.component.css"],
 })
-export class StudentLandingPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class StudentLandingPageComponent {}
