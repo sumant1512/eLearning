56c7ce224afcc1680d6a177150ad062e50703e5e
Sumant Mishra <sumantmishra511@gmail.com>
2020-09-05 12:13:46 +0530

    changes for dynamic student profile

--- src/app/shared/common-profile/common-profile.component.ts
+++ src/app/shared/common-profile/common-profile.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AppState } from "src/app/store/app.state";
+import { Store } from "@ngrx/store";
+import { ProfileType } from "../../store/auth/types/profile.type";
+import * as AuthActions from "../../store/auth/auth.actions";
 
 @Component({
-  selector: 'app-common-profile',
-  templateUrl: './common-profile.component.html',
-  styleUrls: ['./common-profile.component.css']
+  selector: "app-common-profile",
+  templateUrl: "./common-profile.component.html",
+  styleUrls: ["./common-profile.component.css"],
 })
 export class CommonProfileComponent implements OnInit {
-
-  constructor() { }
+  studentProfile: ProfileType;
+  loaded: boolean = false;
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.getUserProfile();
   }
 
+  getUserProfile(): void {
+    this.store.select("profile").subscribe((response) => {
+      if (response.userDetails.user_id !== null) {
+        console.log(response);
+        this.studentProfile = response;
+        this.loaded = true;
+      } else {
+        this.fetchUserProfile();
+      }
+    });
+  }
+
+  fetchUserProfile(): void {
+    const authToken = localStorage.getItem("AUTH_TOKEN");
+    this.store.dispatch(new AuthActions.FetchProfile(authToken));
+  }
 }
