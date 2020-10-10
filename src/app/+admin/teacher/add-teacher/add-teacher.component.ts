c060ffe1da5fe3ff64b8fd55316b32082c4210cd
sumant mishra <sumantmishra511@gmail.com>
2020-10-11 01:53:41 +0530

    encryption added

--- src/app/+admin/teacher/add-teacher/add-teacher.component.ts
+++ src/app/+admin/teacher/add-teacher/add-teacher.component.ts
@@ -1,26 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-import { teacherRegistrationForm } from '../../teacher/add-teacher/add-teacher.utils';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { teacherRegistrationForm } from "../../teacher/add-teacher/add-teacher.utils";
 import { Store } from "@ngrx/store";
 import { AppState } from "src/app/store/app.state";
 @Component({
-  selector: 'app-add-teacher',
-  templateUrl: './add-teacher.component.html',
-  styleUrls: ['./add-teacher.component.css']
+  selector: "app-add-teacher",
+  templateUrl: "./add-teacher.component.html",
+  styleUrls: ["./add-teacher.component.css"],
 })
 export class AddTeacherComponent implements OnInit {
   teacherRegistrationForm: FormGroup;
-  constructor(private store: Store<AppState>) { 
+  constructor(private store: Store<AppState>) {
     this.teacherRegistrationForm = teacherRegistrationForm();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
   get f() {
     return this.teacherRegistrationForm.controls;
   }
-  registerTeacher():void{
-console.log(this.teacherRegistrationForm.value)
-  }
 
+  registerTeacher(): void {
+    console.log(this.teacherRegistrationForm.value);
+  }
 }
