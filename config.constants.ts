638cb2518b42e6bce8d41516f59253d438917fed
sumant mishra <sumantmishra511@gmail.com>
2020-10-12 09:24:08 +0530

    changes in server

--- config.constants.ts
+++ config.constants.ts
@@ -2,4 +2,4 @@ const localServerPort = "http://localhost:3000/";
 const herokuServer = "https://tcslearningapplication.herokuapp.com/";
 const productionServer = "https://elearningserverprd.herokuapp.com/";
 
-export const HOST = localServerPort;
+export const HOST = productionServer;
